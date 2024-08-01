856443f61af38658bafd256908f88f47af6ebc75
Y-admen <yassminash305@gmail.com>
2024-08-01 23:25:23 +0300

    task6.1

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-from typing import List
 "sum list elements"
+
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    "sum of list"
+    """
+    Calculate the sum of a list of floats.
+
+    Args:
+        input_list: A list of floats.
+
+    Returns:
+        The sum of the input list.
+
+    """
     return sum(input_list)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-from typing import Union, List
 "concat str1/str2"
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Calculate the sum of a mixed list of integers and floats.
+
+    Args:
+        mxd_lst (List[Union[int, float]]): A list
+        containing integers and floats.
+
+    Returns:
+        float: The sum of the elements in the mixed list.
+
+    """
     return sum(mxd_lst)
