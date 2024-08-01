e5c635cca127dcafe8347412185e69e7c95ff3a4
Y-admen <yassminash305@gmail.com>
2024-08-01 23:18:15 +0300

    task6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,7 @@
-from typing import Union , List
+#!/usr/bin/env python3
+from typing import Union, List
+"concat str1/str2"
+
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     return sum(mxd_lst)
-print(sum_mixed_list.__annotations__)
-mixed = [5, 4, 3.14, 666, 0.99]
-ans = sum_mixed_list(mixed)
-print(ans == sum(mixed))
-print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
