1434bf121d71bd70536382ca0c6f2f140f6131a0
Y-admen <yassminash305@gmail.com>
2024-08-05 18:57:16 +0300

    concurrent_coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""concurrent_coroutines"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        List[float]: _description_
+    """
+    wait = [await wait_random(max_delay) for _ in range(n)]
+    return wait
