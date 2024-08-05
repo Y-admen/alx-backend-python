73494d03a8ab580d684932c967e67543ab0962b1
Y-admen <yassminash305@gmail.com>
2024-08-05 19:45:49 +0300

    measure_runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """_summary_
 
     Args:
@@ -16,5 +16,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         List[float]: _description_
     """
     wait = [await wait_random(max_delay) for _ in range(n)]
-    results = wait.sort()
+    wait.sort()
     return wait
