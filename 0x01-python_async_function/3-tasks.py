c03e082d4e8aa53f4a16f6a679c650c12e327cd2
Y-admen <yassminash305@gmail.com>
2024-08-05 21:07:33 +0300

    tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""tasks"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delayL: int):
+    """_summary_
+
+    Args:
+        max_delayL (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    task = asyncio.create_task(wait_random())
+    return task
