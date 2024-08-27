0313bd5a54709a0aa0a8641d59779dfde1379e75
Y-admen <yassminash305@gmail.com>
2024-08-27 04:49:18 +0300

    KeyErro

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,11 @@ class TestAccessNestedMap(unittest.TestCase):
         "test access nested map"
         self.assertequal(access_nested_map(nested_map, path, expected))
 
-    @parameterized.expand([({}, ("a",), KeyError),
-                           ({"a": 1}, ("a", "b"), KeyError)])
-    def test_access_nested_map_exception(self, nested_map, path, expected):
-        self.assertRaise(nested_map, path, expected)
+    @parameterized.expand([({}, ("a",), "a"),
+                           ({"a": 1}, ("a", "b"), "b")])
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_excep_msg):
+        "test access nested map exception"
+        with self.assertRaise(KeyError) as err:
+            access_nested_map(nested_map, path)
+        self.assertEqual(err.exception.args[0], expected_excep_msg)
